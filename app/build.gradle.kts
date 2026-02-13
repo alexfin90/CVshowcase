a565efca3211a89f28fb6d34ab234d09f12cd03a
Alessandro Finocchiaro <afinocchiaro@technogym.com>
2026-02-13 13:00:04 +0400

    feat: add Firebase dependencies and configure Google services in Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
     alias(libs.plugins.cvshowcase.application)
     alias(libs.plugins.cvshowcase.flavors)
+    alias(libs.plugins.google.services)
 }
 
 android {
     namespace = project.applicationPackage
+}
+
+
+dependencies{
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
