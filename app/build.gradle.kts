eff30b42e3777b0a9a5b9f499a0d01ca8d4a8515
Alessandro Finocchiaro <afinocchiaro@technogym.com>
2026-02-13 13:29:24 +0400

    feat: add Firebase Crashlytics dependencies and configure in Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.cvshowcase.application)
     alias(libs.plugins.cvshowcase.flavors)
     alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
 }
 
 android {
@@ -12,4 +13,6 @@ android {
 dependencies{
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics)
+    implementation(libs.google.firebase.analytics)
 }
