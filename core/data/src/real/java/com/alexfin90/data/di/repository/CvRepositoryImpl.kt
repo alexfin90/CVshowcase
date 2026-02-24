af9d73f65a94190d51696c39fd7b9d099f4df2ba
Alessandro Finocchiaro <afinocchiaro@technogym.com>
2026-02-24 15:17:57 +0100

    feat: integrate IoDispatcher into CvRepositoryImpl and update hiltViewModel import

--- core/data/src/real/java/com/alexfin90/data/di/repository/CvRepositoryImpl.kt
+++ core/data/src/real/java/com/alexfin90/data/di/repository/CvRepositoryImpl.kt
@@ -4,17 +4,24 @@ import com.alexfin90.data.mapper.toDomain
 import com.alexfin90.data.remote.CvApiService
 import com.alexfin90.domain.contracts.CvRepository
 import com.alexfin90.domain.entities.Cv
+import com.technogym.android.absolute.strength.core.didispatcher.IoDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CvRepositoryImpl @Inject constructor(
+    @param:IoDispatcher private val dispatcher: CoroutineDispatcher,
     private val api: CvApiService
 ) : CvRepository {
 
 
     override suspend fun getCv(): Cv {
-       return api.getCv().toDomain()
+       return withContext(dispatcher){
+            api.getCv().toDomain()
+       }
     }
 
     override fun observeCv(): Flow<Cv> = flow {
